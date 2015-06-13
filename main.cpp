bae874cdee0cd074a3b809bacc21e445a2dad98d
Sorin <sorin@wepay.com>
2015-06-13 10:12:10 +0300

    Changes

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include "Matrix.h"
 #include "tests.cpp"
 
 using namespace std;
 
 int main() {
 
-    if (tests() > 0) {
+    int testsResult = tests();
+
+    if (testsResult > 0) {
         cout << "Tests failed" << endl;
     } else {
         cout << "Tests passed" << endl;
     }
 
-    return 0;
+    return testsResult;
 }
+
